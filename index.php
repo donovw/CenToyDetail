bf0fb082caee2edcb19ef4b80b2d5a9a6e7b6923
Dono <dono@dfn.online>
2019-11-01 12:57:50 -0700

    Added search form on index page

--- index.php
+++ index.php
@@ -2,5 +2,9 @@
 <main>
   <div id="vehsearch">
     <input type="text" name="vin-stk-search-box" value="" placeholder="Enter VIN or Stock#">
+    <div id="searchbtns">
+    <button type="button" id="searchbtn1" name="btn-search-vin">Search by VIN</button>
+    <button type="button" id="searchbtn2" name="btn-search-stk">Search by Stock</button>
+    </div>
   </div>
 </main>
